a9ecaec6b36b2dc649b0c8e430e67f59ce6f4427
Mikhail Smirnov <smrnvmkhl@gmail.com>
2019-03-26 14:25:27 +0300

    Add androidx support

--- android/src/main/java/com/taluttasgiran/rnsecurestorage/RNSecureStorageModule.java
+++ android/src/main/java/com/taluttasgiran/rnsecurestorage/RNSecureStorageModule.java
@@ -2,7 +2,7 @@ package com.taluttasgiran.rnsecurestorage;
 
 import android.content.SharedPreferences;
 import android.os.Build;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.react.bridge.Promise;
